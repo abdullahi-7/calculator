a577982c212b844ac43b7435e5fe773da1547350
abdullahi-7 <abdullahi.abdi@protonmail.com>
2021-08-22 07:01:57 +0300

    finished project

--- calculator.py
+++ calculator.py
@@ -1,3 +1,38 @@
-print("Hello world")
-# variable with my name
-name = "Khadija"
+import time
+# The mining of import is something is called
+print("my name is Abdulla.\nI use python to write it.\nwellcome to use it")
+print("if you want add click S.\nif you want subtract click S\nif you want to multiply click X\nif you want to divide click D")
+
+choice = input("\wich letter?\n").upper()
+
+numberX = int(input("write the first number: "))
+# The numberX is variable
+numberY = int(input("write the seconds number"))
+
+multiply = "X"
+add = "A"
+subtract = "S"
+divide = "D"
+
+
+if choice == add:
+    answer = numberX + numberY
+    print(answer)
+elif choice == subtract:
+    answer = numberX - numberY
+    print(answer)
+elif choice == multiply:
+    answer = numberX * numberY
+    print(answer)
+elif choice == divide:
+    answer = numberX / numberY
+    print(answer)
+else:
+    print("use the intruction above")
+
+
+print("plese wait for 5.....seconds")
+
+
+
+time.sleep(5)
